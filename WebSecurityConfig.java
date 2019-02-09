a625563ec23150adf2e1e8c5fd5c519d43e28f81
KalyanGudala <kalyan.gudala@gmail.com>
2019-02-09 00:32:36 -0800

    Version1.0 For Spring Security Documentation

---
+++ WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.spring5.demo.config;
+
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+		.withUser("admin").password("$2a$10$PX3hZUt4QX9OuCzFotwu4exRHYussHJnpLvNxNBkUTw8d2tDO4yse").roles("ADMIN")
+		.and()
+		.passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests().antMatchers("/").hasAnyRole("ADMIN")
+		.and()
+		.formLogin()
+		.and()
+		.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.and()
+		.csrf().disable();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("admin123"));
+	}
+
+}
